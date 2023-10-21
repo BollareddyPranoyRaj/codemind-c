182371231b077331b4b88f10dbdd42cf46cff582
BollareddyPranoyRaj <147791767+BollareddyPranoyRaj@users.noreply.github.com>
2023-10-21 08:35:58 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(x==1)     printf("Monday");     else if(x==2)     printf("Tuesday");     else if(x==3)     printf("Wednesday");     else if(x==4)     printf("Thursday");     else if(x==5)     printf("Friday");     else if(x==6)     printf("Saturday");     else     printf("Invalid input"); }

---
+++ Day_name__.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x==1)
+    printf("Monday");
+    else if(x==2)
+    printf("Tuesday");
+    else if(x==3)
+    printf("Wednesday");
+    else if(x==4)
+    printf("Thursday");
+    else if(x==5)
+    printf("Friday");
+    else if(x==6)
+    printf("Saturday");
+    else 
+    printf("Invalid input");
+}
