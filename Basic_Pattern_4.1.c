1f096a20c1d6f143735545140692f59481875c28
BollareddyPranoyRaj <147791767+BollareddyPranoyRaj@users.noreply.github.com>
2023-12-22 09:51:05 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i%2==1)             printf("0 ");             if(i%2==0)             printf("1 ");         }         printf(" ");     } }

---
+++ Basic_Pattern_4.1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i%2==1)
+            printf("0 ");
+            if(i%2==0)
+            printf("1 ");
+        }
+        printf("
+");
+    }
+}
