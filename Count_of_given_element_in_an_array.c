cef83c0caedf3b1d06b6dd9dacc0cc9fa722984f
BollareddyPranoyRaj <147791767+BollareddyPranoyRaj@users.noreply.github.com>
2024-01-01 10:33:20 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int a,c=0;     scanf("%d",&a);     for(i=0;i<n;i++)     {         if(a==arr[i])         {             c=c+1;         }     }     printf("%d",c); }

---
+++ Count_of_given_element_in_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int a,c=0;
+    scanf("%d",&a);
+    for(i=0;i<n;i++)
+    {
+        if(a==arr[i])
+        {
+            c=c+1;
+        }
+    }
+    printf("%d",c);
+}
